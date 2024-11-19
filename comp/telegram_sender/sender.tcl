889b8c79b82fa1bb8712fdee40ee8f9c420ff347
Themen <thesolve@mail.ru>
2024-11-19 16:13:13 +0300

    Небольшие улучшения кода.

--- comp/telegram_sender/sender.tcl
+++ comp/telegram_sender/sender.tcl
@@ -1,27 +1,31 @@
 #!/usr/bin/tclsh
+#package provide telegram_sender 1.0
 package require http
 package require tls
 
 http::register https 443 ::tls::socket
 http::config -useragent {Tcl telegram bot sender}
 
-set MESSAGE {Receive poweroff command.}
+set MESSAGE qwqw
+#[lindex $argv 0]
+puts "Message: $MESSAGE"
 
-after 0 {
+after 0 {SendMessageToTelegram $MESSAGE}
+
+proc SendMessageToTelegram {message} {
 	array set cred [getCredentials]
-	set url "https://api.telegram.org/bot$cred(bot_token)/sendMessage"
-	#set url {http://127.0.0.1}
-	set body "{\"text\": \"$MESSAGE\", \"chat_id\": \"$cred(chat_id)\"}"
+	#set url "https://api.telegram.org/bot$cred(bot_token)/sendMessage"
+	set url {http://127.0.0.1}
+	set body "{\"text\": \"$message\", \"chat_id\": \"$cred(chat_id)\"}"
 	set contentType application/json
 	post $url $body $contentType onTelegramServerRespond
 }
 
 proc onTelegramServerRespond {body} {
-	
 	if {[hasOkInRespond $body]} {
 		puts {Message successfully sent to telegram.}
 	} else {
-		puts {The server reported an error while sending a message to Telegram.}
+		puts stderr {The server reported an error while sending a message to Telegram.}
 	}
 }
 
@@ -29,8 +33,10 @@ proc hasOkInRespond {body} {
 	set isOk false
 	foreach el [split $body ,] {
 		set keyValue [split $el :]
-		if {[string equal [lindex $keyValue 0] \"ok\"]} {
-			if {[string equal [lindex $keyValue 1] true] || [string equal [lindex $keyValue 1] \"true\"]} {
+		set key [lindex $keyValue 0]
+		if {[string equal $key \"ok\"]} {
+			set value [lindex $keyValue 1]
+			if {[string equal $value true] || [string equal $value \"true\"]} {
 				set isOk true
 			} else {
 				break
@@ -43,11 +49,11 @@ proc hasOkInRespond {body} {
 proc post {url body contenType callback} {
 	catch {http::geturl $url -headers [list Content-Type $contenType] -query $body -command [list onAnswer $callback]} errorOrToken
 	if {[string first ::http:: $errorOrToken] == -1} {
-		puts $errorOrToken
+		puts stderr $errorOrToken
 		exit 1
 	}
 	after 5000 {
-		puts {Server is not respond}
+		puts stderr {Server is not respond}
 		if {[info exist errorOrToken]} {
 			http::reset $errorOrToken
 		}
@@ -59,7 +65,7 @@ proc post {url body contenType callback} {
 		if {[string match *200* $res(http)]} {
 			uplevel #0 $callback $res(body)
 		} else {
-			puts "Server respond with error: $res(http)"
+			puts stderr "Server respond with error: $res(http)"
 			exit 3
 		}
 		http::cleanup $token
@@ -68,7 +74,8 @@ proc post {url body contenType callback} {
 }
 
 proc getCredentials {} {
-	set credStream [open credentials.txt r]
+	set pathToCredentials [file join [file dirname [info script]] credentials.txt]
+	set credStream [open $pathToCredentials r]
 	set values {}
 	while {![eof $credStream]} {
 		set row [gets $credStream]
