b5f49bbac0de1c9002749b57fda14b6b7b2bc8df
Themen <thesolve@mail.ru>
2024-11-19 11:47:05 +0300

    Закончил скрипт отправки сообщений в телеграм.

--- comp/sender/send.tcl
+++ comp/sender/send.tcl
@@ -2,20 +2,70 @@
 package require http
 package require tls
 
+http::register https 443 ::tls::socket
+http::config -useragent {Tcl telegram bot sender}
+
+set MESSAGE {Receive poweroff command.}
+
 after 0 {
 	array set cred [getCredentials]
-	puts $cred(chat_id)
+	set url "https://api.telegram.org/bot$cred(bot_token)/sendMessage"
+	#set url {http://127.0.0.1}
+	set body "{\"text\": \"$MESSAGE\", \"chat_id\": \"$cred(chat_id)\"}"
+	set contentType application/json
+	post $url $body $contentType onTelegramServerRespond
+}
 
-	http::register https 443 ::tls::socket
-	set token [http::geturl https://mendeo.ru]
-	puts $token
-	upvar 0 $token qq
-	foreach {key value} [array get qq] {
-		puts "$key: $value"
+proc onTelegramServerRespond {body} {
+	
+	if {[hasOkInRespond $body]} {
+		puts {Message successfully sent to telegram.}
+	} else {
+		puts {The server reported an error while sending a message to Telegram.}
 	}
-	http::cleanup $token
-	exitNormal
-} {}
+}
+
+proc hasOkInRespond {body} {
+	set isOk false
+	foreach el [split $body ,] {
+		set keyValue [split $el :]
+		if {[string equal [lindex $keyValue 0] \"ok\"]} {
+			if {[string equal [lindex $keyValue 1] true] || [string equal [lindex $keyValue 1] \"true\"]} {
+				set isOk true
+			} else {
+				break
+			}
+		}
+	}
+	return $isOk
+}
+
+proc post {url body contenType callback} {
+	catch {http::geturl $url -headers [list Content-Type $contenType] -query $body -command [list onAnswer $callback]} errorOrToken
+	if {[string first ::http:: $errorOrToken] == -1} {
+		puts $errorOrToken
+		exit 1
+	}
+	after 5000 {
+		puts {Server is not respond}
+		if {[info exist errorOrToken]} {
+			http::reset $errorOrToken
+		}
+		exit 2
+	}
+
+	proc onAnswer {callback token} {
+		upvar #0 $token res
+		if {[string match *200* $res(http)]} {
+			uplevel #0 $callback $res(body)
+		} else {
+			puts "Server respond with error: $res(http)"
+			exit 3
+		}
+		http::cleanup $token
+		exitNormal
+	}
+}
 
 proc getCredentials {} {
 	set credStream [open credentials.txt r]
